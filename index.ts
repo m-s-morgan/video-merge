11f170be559b43afce1e50b6fa50fbd99d263506
Matt Morgan <m@getplatypi.com>
2021-03-03 13:16:33 -0500

    fixing unhandled promise issues

--- index.ts
+++ index.ts
@@ -20,12 +20,21 @@ function isEmpty(obj: any) {
     return obj === '' || isNull(obj);
 }
 
+function isObject(obj: any) {
+    return obj != null && typeof obj === 'object';
+}
+
 function isArray(obj: any) {
     return Object.prototype.toString.call(obj) === '[object Array]';
 }
 
 function onError(err: Error) {
-    console.error(err);
+    if (isObject(err)) {
+        console.error(`ERROR: ${err.message}`, '\n');
+    } else {
+        console.error(err, '\n');
+    }
+
     process.exitCode = 1;
 }
 
@@ -44,37 +53,41 @@ function merge(pre: string, input: string) {
     });
 }
 
-try {
-    const prerollFiles = await fsPromises.readdir(FOLDERS.PREROLL);
+async function mergeAll() {
+    try {
+        const prerollFiles = await fsPromises.readdir(FOLDERS.PREROLL);
 
-    if (!isArray(prerollFiles) || prerollFiles.length === 0) {
-        throw new Error('Please add a preroll video to the preroll folder');
-    }
-
-    let preroll: string;
-
-    for (const p of prerollFiles) {
-        const stat = await fsPromises.stat(p);
-
-        if (!stat.isDirectory()) {
-            preroll = p;
-            break;
+        if (!isArray(prerollFiles) || prerollFiles.length === 0) {
+            throw new Error('Please add a preroll video to the preroll folder');
         }
-    }
 
-    if (isEmpty(preroll)) {
-        throw new Error('Please add a preroll video to the preroll folder');
-    }
+        let preroll: string;
 
-    const inputFiles = await fsPromises.readdir(FOLDERS.INPUT);
+        for (const p of prerollFiles) {
+            const stat = await fsPromises.stat(p);
 
-    for (const i of inputFiles) {
-        const stat = await fsPromises.stat(i);
-
-        if (!stat.isDirectory()) {
-            console.log(await merge(preroll, i));
+            if (!stat.isDirectory()) {
+                preroll = p;
+                break;
+            }
         }
+
+        if (isEmpty(preroll)) {
+            throw new Error('Please add a preroll video to the preroll folder');
+        }
+
+        const inputFiles = await fsPromises.readdir(FOLDERS.INPUT);
+
+        for (const i of inputFiles) {
+            const stat = await fsPromises.stat(i);
+
+            if (!stat.isDirectory()) {
+                console.log(await merge(preroll, i));
+            }
+        }
+    } catch (e) {
+        onError(e);
     }
-} catch (e) {
-    onError(e);
 }
+
+mergeAll();
